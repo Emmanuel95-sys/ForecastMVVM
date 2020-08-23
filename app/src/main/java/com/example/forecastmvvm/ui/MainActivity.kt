e027fd8e6efd61cacde8b7c8b2d9e1ffb3d60085
Emmanuel Peña <1em.ppe.35@gmail.com>
2020-08-22 23:54:33 -0500

    setting nav controller bottom nav, toolbar action bar, also onSupportNavigate up

--- app/src/main/java/com/example/forecastmvvm/ui/MainActivity.kt
+++ app/src/main/java/com/example/forecastmvvm/ui/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.forecastmvvm.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.forecastmvvm.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //set toolbar
+        setSupportActionBar(toolbar)
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        //set navigation menu with nav controller
+        bottom_nav.setupWithNavController(navController)
+       //set up action bar with nav controller
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
+    }
+
 }
