1e7106889e4469d578379ae18903f079395b7f8f
Emmanuel Peña <1em.ppe.35@gmail.com>
2020-09-08 17:59:33 -0500

    using our lazyDeferred method and building our CurrentWeatherViewmodelFactory

--- app/src/main/java/com/example/forecastmvvm/ui/weather/current/CurrentWeatherViewModel.kt
+++ app/src/main/java/com/example/forecastmvvm/ui/weather/current/CurrentWeatherViewModel.kt
@@ -3,6 +3,7 @@ package com.example.forecastmvvm.ui.weather.current
 import androidx.lifecycle.ViewModel
 import com.example.forecastmvvm.data.repository.ForecastRepository
 import com.example.forecastmvvm.internal.UnitSystem
+import com.example.forecastmvvm.internal.lazyDeferred
 
 class CurrentWeatherViewModel(
     private val forecastRepository: ForecastRepository
@@ -11,7 +12,7 @@ class CurrentWeatherViewModel(
     val isMetric: Boolean
         get() = unitSystem == UnitSystem.METRIC
 
-    val weather by lazy {
+    val weather by lazyDeferred {
         forecastRepository.getCurrentWeather(isMetric)
     }
     
